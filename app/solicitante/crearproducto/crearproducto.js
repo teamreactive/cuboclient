e43c2ac89bc575f523a2cef42820da0f1c01b3e3
jcbages <jcbages@outlook.com>
2014-11-23 00:35:32 -0500

    all

--- app/solicitante/crearproducto/crearproducto.js
+++ app/solicitante/crearproducto/crearproducto.js
@@ -1,6 +1,28 @@
 angular.module("crearproducto", ["crud"])
 .controller("CrearProductoController", ["$scope", "$http", "service", function($scope, $http, service) {
+	$scope.producto = {};
 
+	$scope.quitarnombre = function(nombre) {
+		var i = $scope.producto.nombres.indexOf(nombre);
+		$scope.producto.nombres.splice(i, 1);
+	}
+
+	$scope.agregarnombre = function() {
+		$scope.producto.nombres.push("");
+	}
+
+	$scope.quitarunidad = function(unidad) {
+		var i = $scope.producto.unidades.indexOf(unidad);
+		$scope.producto.unidades.splice(i, 1);
+	}
+
+	$scope.agregarunidad = function() {
+		$scope.producto.unidades.push("");
+	}
+
+	$scope.crearproducto = function() {
+		// TODO
+	}
 }])
 .directive("crearproducto", function() {
 	return {
